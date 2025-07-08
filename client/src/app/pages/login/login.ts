39316a1f910c6a65e280267b2d39183529535150
Dempsey <dagantareynat@gmail.com>
2025-07-09 06:45:53 +0800

    feat: add login API integration

--- client/src/app/pages/login/login.ts
+++ client/src/app/pages/login/login.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  email = '';
+  password = '';
+  error = '';
+  message = '';
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  onLoginSubmit() {
+    this.auth.login({ email: this.email, password: this.password }).subscribe({
+      next: (res) => {
+        this.error = '';
+        this.message = 'Login successful! Redirecting...';
+        this.auth.storeToken(res.token);
+        setTimeout(() => this.router.navigate(['/lobby']), 1500);
+      },
+      error: (err) => {
+        this.message = '';
+        this.error = err?.error || 'Login failed. Check credentials.';
+      }
+    });
+  }
 }
