543f6f88ce30814be4ee5216080557d96d1c7629
elderfieldzeus <elderfieldzeus24@gmail.com>
2025-07-10 18:08:16 +0800

    refactor: changed signup page

--- client/src/app/pages/signup/signup.ts
+++ client/src/app/pages/signup/signup.ts
@@ -4,13 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth';
 import { ToastrService } from 'ngx-toastr';
+import { InputField } from '../../components/input-field/input-field';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink, FormsModule, CommonModule],
+  imports: [RouterLink, FormsModule, CommonModule, InputField],
   templateUrl: './signup.html',
-  styleUrl: './signup.css'
+  styleUrl: './signup.css',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [ // when element enters the DOM
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
 })
 export class Signup {
   email = '';
@@ -18,8 +28,15 @@ export class Signup {
   confirmPassword = '';
   message = '';
   error = '';
+  showElement = false;
 
   constructor(private auth: AuthService, private router: Router, private toastr: ToastrService) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.showElement = true;
+    }, 100);
+  }
 
   onSignupSubmit() {
     if (this.password !== this.confirmPassword) {
