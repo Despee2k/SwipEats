c2bd6f118d5e9bcd5ccb89ce46e4ea7c172cce83
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-07-17 12:12:51 +0800

    test: changed ws to wss

--- client/src/app/utils/constant.ts
+++ client/src/app/utils/constant.ts
@@ -4,6 +4,6 @@ export const PROD_URL = 'swipeats-api.dcism.org';
 export const LOCAL_URL = 'localhost:42562';
 
 export const BASE_API_URL = PRODUCTION ? `https://${PROD_URL}` : `http://${LOCAL_URL}`;
-export const BASE_WS_URL = `ws://${PRODUCTION ? PROD_URL : LOCAL_URL}`;
+export const BASE_WS_URL = PRODUCTION ? `wss://${PROD_URL}` : `ws://${LOCAL_URL}`;
 
 export const API_URL_V1 = `${BASE_API_URL}/api/v1`;
